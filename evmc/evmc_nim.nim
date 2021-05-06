bd5b88d93a8f30005225e3d32a4dc5a75343c628
Jamie Lokier <jamie@shareable.org>
2021-05-06 17:33:47 +0100

    Nim: Add and update copyright headers
    Signed-off-by: Jamie Lokier <jamie@shareable.org>

--- evmc/evmc_nim.nim
+++ evmc/evmc_nim.nim
@@ -1,3 +1,13 @@
+# Nimbus - EVMC binary compatible interface
+#
+# Copyright (C) 2018-2019, 2021 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option. This file may not be copied, modified, or distributed except
+# according to those terms.
+
+
 import evmc
 
 type
